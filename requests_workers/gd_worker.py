e7273b201ab1d359fa797361cf8a84126a0719ea
megahomyak <g.megahomyak@gmail.com>
2021-02-07 00:10:48 +0500

    Made GDWorker

---
+++ requests_workers/gd_worker.py
@@ -0,0 +1,34 @@
+import gd
+
+
+class LevelNotFound(Exception):
+    pass
+
+
+class GDWorker:
+
+    def __init__(self, gd_client: gd.Client):
+        self.gd_client = gd_client
+
+    async def get_player(self, player_name: str) -> gd.User:
+        """
+        Can throw gd.MissingAccess
+        """
+        player = await self.gd_client.find_user(player_name)
+        full_player = await player.to_user()
+        return full_player
+
+    async def get_level_by_id(self, level_id: int) -> gd.Level:
+        """
+        Can throw gd.MissingAccess
+        """
+        return await self.gd_client.get_level(level_id)
+
+    async def get_level_by_name(self, level_name: str) -> gd.Level:
+        """
+        Can throw LevelNotFound
+        """
+        levels = await self.gd_client.search_levels(level_name, pages=[0])
+        if levels:
+            return levels[0]
+        raise LevelNotFound(f"Level with name {level_name} not found!")
